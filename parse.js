2ed969cce92ef7e620deab988e2d3ba11397367c
Umut Cetin <umutc3@gmail.com>
2018-03-28 22:28:30 +0300

    json url

--- parse.js
+++ parse.js
@@ -1,4 +1,4 @@
-$.getJSON("http://127.0.0.1:8080/test.json", function (data) {
+$.getJSON("test.json", function (data) {
 	var items = [];
 	for (var i = 0; i < data.length; i++) {
 		items.push("<li id='" + data[i].sentence + "'>" + data[i].sentence + "</li>");
